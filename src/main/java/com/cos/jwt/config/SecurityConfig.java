4ef40f091e29fef04f18cc165b390148da4aae67
seoyoung1013 <lemi01@naver.com>
2023-10-19 00:19:41 +0900

    jwt Bearer 인증 방식

---
+++ src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.cos.jwt.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+import lombok.RequiredArgsConstructor;
+
+
+@Configuration
+@EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final CorsFilter corsFilter;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.addFilter(corsFilter) //@CrossOrigin(인증x), 시큐리티 필터에 등록 인증(o)
+		.formLogin().disable()
+		.httpBasic().disable()
+		.authorizeRequests()
+		.antMatchers("/api/v1/user/**")
+		.access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/api/v1/manager/**")
+			.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/api/v1/admin/**")
+			.access("hasRole('ROLE_ADMIN')")
+		.anyRequest().permitAll();
+	}
+}
