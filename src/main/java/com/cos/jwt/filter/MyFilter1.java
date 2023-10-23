044ee154535dc538742a3c98cf2a15bfbd42f4b6
seoyoung1013 <lemi01@naver.com>
2023-10-23 18:20:14 +0900

    jwt 임시토큰 만들어서 테스트 해보기

--- src/main/java/com/cos/jwt/filter/MyFilter1.java
+++ src/main/java/com/cos/jwt/filter/MyFilter1.java
@@ -7,16 +7,25 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class MyFilter1 implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		System.out.println("필터1");
-		chain.doFilter(request, response);
 		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String headerAuth = req.getHeader("Authorization");
+		System.out.println(headerAuth);
+		 
+		if(req.getMethod().equals("GET")) {
+			
+		}
+		
+		System.out.println("필터1");
+		chain.doFilter(req, res);
 	}
-	
-
 }
